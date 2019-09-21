b40b40dadbc3e7aa884316ba67796650314e464d
Clayton <claystamper@gmail.com>
2019-09-21 18:26:16 -0500

    reading in controller input

--- Jam2019/Assets/Scripts/Gun.cs
+++ Jam2019/Assets/Scripts/Gun.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.MagicLeap;
 
 public class Gun : MonoBehaviour {
 
+    [SerializeField] private GameObject bullet;
     [SerializeField] private Transform axis;
 
+    private void Start() {
+        MLInput.OnTriggerDown += OnTriggerDown;
+    }
+
+    private void OnTriggerDown(byte controllerId, float value) {
+        Debug.Break();
+    }
+    
     void Update()
     {
         
