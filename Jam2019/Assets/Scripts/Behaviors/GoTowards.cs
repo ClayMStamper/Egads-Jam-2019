c522db0e82dd66543229f2abade07a336405640e
Clayton <claystamper@gmail.com>
2019-09-21 12:43:06 -0500

    downward direction

---
+++ Jam2019/Assets/Scripts/Behaviors/GoTowards.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Behavior/Goto")]
+public class GoTowards : FlockBehavior {
+
+    private Transform target;
+    
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
+
+        if (!target) 
+            GetTarget();
+
+        Vector3 targetDir = (target.position - agent.transform.position).normalized;
+
+        return targetDir;
+
+    }
+
+    private void GetTarget() {
+        target = GraveManager.instance.GetTarget();
+    }
+    
+}
