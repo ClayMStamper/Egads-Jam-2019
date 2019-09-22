1fca358aeabb92a4f8c264110837e1a1a34d0e11
Justin Cupial <jpc138@txstate.edu>
2019-09-21 21:43:46 -0500

    adding the intro sequence

--- Jam2019/Assets/Scripts/Gun.cs
+++ Jam2019/Assets/Scripts/Gun.cs
@@ -3,7 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.MagicLeap;
 
+<<<<<<< Updated upstream
 public class Gun : MonoBehaviour {
+=======
+public class Gun : MonoBehaviour
+{
+    public Transform bullet;
+    public Transform spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    
+>>>>>>> Stashed changes
 
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform axis;
@@ -13,8 +28,14 @@ public class Gun : MonoBehaviour {
         MLInput.OnTriggerDown += OnTriggerDown;
     }
 
+<<<<<<< Updated upstream
     private void OnTriggerDown(byte controllerId, float value) {
         Destroy(Instantiate(bullet, axis.position, axis.rotation), bulletLifetime);
+=======
+    void Update()
+    {
+
+>>>>>>> Stashed changes
     }
 
 }
