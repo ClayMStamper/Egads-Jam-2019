e1a976cb4e8ac9ccc1c5cc3d03e7e8c3185f37f1
elijah gaytan <elijahgaytan@utexas.edu>
2019-09-22 11:06:52 -0500

    final change to behaviors for agents

--- Jam2019/Assets/Scripts/Behaviors/GoTowards.cs
+++ Jam2019/Assets/Scripts/Behaviors/GoTowards.cs
@@ -7,12 +7,12 @@ using UnityEngine;
 public class GoTowards : FlockBehavior {
 
     //private Transform flockAgentTarget;
-    
+    [SerializeField] private float maxSpeed = 1f;
 
     public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, FlockManager flockManager) {
         
         Vector3 targetDir = (agent.target.position - agent.transform.position).normalized;
-        
+        FindObjectOfType<FlockManager>().maxSpeed = maxSpeed;
         return targetDir;
 
     }
