3815e46b54cc6fb93d8de0075ba1719760c93751
Clayton <claystamper@gmail.com>
2019-09-21 19:01:21 -0500

    mend

--- Jam2019/Assets/Scripts/Gun.cs
+++ Jam2019/Assets/Scripts/Gun.cs
@@ -3,31 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.MagicLeap;
 
-public class Gun : MonoBehaviour
-{
-    public Transform bullet;
-    public Transform spawn;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 public class Gun : MonoBehaviour {
 
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform axis;
+    [SerializeField] private float bulletLifetime = 2;
 
     private void Start() {
         MLInput.OnTriggerDown += OnTriggerDown;
     }
 
     private void OnTriggerDown(byte controllerId, float value) {
-        Debug.Break();
+        Destroy(Instantiate(bullet, axis.position, axis.rotation), bulletLifetime);
     }
-    
-    void Update()
-    {
-      
-    }
+
 }
