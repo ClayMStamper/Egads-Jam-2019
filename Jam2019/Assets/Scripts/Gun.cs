67903bab33a409b69a97ce57ca512a9f5bc1ab1e
Clayton <claystamper@gmail.com>
2019-09-21 22:12:00 -0500

    unadded scenes from gitignore

--- Jam2019/Assets/Scripts/Gun.cs
+++ Jam2019/Assets/Scripts/Gun.cs
@@ -3,24 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.MagicLeap;
 
-<<<<<<< Updated upstream
-public class Gun : MonoBehaviour {
-=======
 public class Gun : MonoBehaviour
 {
     public Transform bullet;
     public Transform spawn;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    
->>>>>>> Stashed changes
-
-    [SerializeField] private GameObject bullet;
     [SerializeField] private Transform axis;
     [SerializeField] private float bulletLifetime = 2;
 
@@ -28,14 +14,8 @@ public class Gun : MonoBehaviour
         MLInput.OnTriggerDown += OnTriggerDown;
     }
 
-<<<<<<< Updated upstream
     private void OnTriggerDown(byte controllerId, float value) {
         Destroy(Instantiate(bullet, axis.position, axis.rotation), bulletLifetime);
-=======
-    void Update()
-    {
-
->>>>>>> Stashed changes
     }
 
 }
